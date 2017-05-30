d6c6d532e194aeea8f3f26125b6b8410e5e5095f
nicolas.torion@insa-rouen.fr <nicolas.torion@insa-rouen.fr>
2017-05-30 12:35:05 +0200

    article new post type

--- functions.php
+++ functions.php
@@ -5,89 +5,99 @@ function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
-function my_custom_post_product() {
+/* New Post Type */
+
+function custom_post_article() {
   $labels = array(
-    'name'               => _x( 'Products', 'post type general name' ),
-    'singular_name'      => _x( 'Product', 'post type singular name' ),
+    'name'               => _x( 'Articles', 'post type general name' ),
+    'singular_name'      => _x( 'Article', 'post type singular name' ),
     'add_new'            => _x( 'Add New', 'book' ),
-    'add_new_item'       => __( 'Add New Product' ),
-    'edit_item'          => __( 'Edit Product' ),
-    'new_item'           => __( 'New Product' ),
-    'all_items'          => __( 'All Products' ),
-    'view_item'          => __( 'View Product' ),
-    'search_items'       => __( 'Search Products' ),
-    'not_found'          => __( 'No products found' ),
-    'not_found_in_trash' => __( 'No products found in the Trash' ),
+    'add_new_item'       => __( 'Add New Article' ),
+    'edit_item'          => __( 'Edit Article' ),
+    'new_item'           => __( 'New Article' ),
+    'all_items'          => __( 'All Articles' ),
+    'view_item'          => __( 'View Article' ),
+    'search_items'       => __( 'Search Articles' ),
+    'not_found'          => __( 'No articles found' ),
+    'not_found_in_trash' => __( 'No articles found in the Trash' ),
     'parent_item_colon'  => '',
-    'menu_name'          => 'Products'
+    'menu_name'          => 'Articles'
   );
   $args = array(
     'labels'        => $labels,
-    'description'   => 'Holds our products and product specific data',
+    'description'   => 'Holds our articles and product specific data',
     'public'        => true,
     'menu_position' => 5,
-    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+    'supports'      => array( 'title', 'editor', 'comments' ),
     'has_archive'   => true,
   );
-  register_post_type( 'product', $args );
+  register_post_type( 'article', $args );
 }
-add_action( 'init', 'my_custom_post_product' );
+add_action( 'init', 'custom_post_article' );
 
+/* Messages */
 
-function my_updated_messages( $messages ) {
+function updated_messages( $messages ) {
   global $post, $post_ID;
-  $messages['product'] = array(
+  $messages['article'] = array(
     0 => '',
-    1 => sprintf( __('Product updated. <a href="%s">View product</a>'), esc_url( get_permalink($post_ID) ) ),
+    1 => sprintf( __('Article updated. <a href="%s">View article</a>'), esc_url( get_permalink($post_ID) ) ),
     2 => __('Custom field updated.'),
     3 => __('Custom field deleted.'),
-    4 => __('Product updated.'),
-    5 => isset($_GET['revision']) ? sprintf( __('Product restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-    6 => sprintf( __('Product published. <a href="%s">View product</a>'), esc_url( get_permalink($post_ID) ) ),
+    4 => __('Article updated.'),
+    5 => isset($_GET['revision']) ? sprintf( __('Article restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+    6 => sprintf( __('Article published. <a href="%s">View product</a>'), esc_url( get_permalink($post_ID) ) ),
     7 => __('Product saved.'),
-    8 => sprintf( __('Product submitted. <a target="_blank" href="%s">Preview product</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
-    9 => sprintf( __('Product scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview product</a>'), date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
-    10 => sprintf( __('Product draft updated. <a target="_blank" href="%s">Preview product</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+    8 => sprintf( __('Article submitted. <a target="_blank" href="%s">Preview article</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+    9 => sprintf( __('Article scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview article</a>'), date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+    10 => sprintf( __('Article draft updated. <a target="_blank" href="%s">Preview article</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
   );
   return $messages;
 }
-add_filter( 'post_updated_messages', 'my_updated_messages' );
+add_filter( 'post_updated_messages', 'updated_messages' );
 
-function my_contextual_help( $contextual_help, $screen_id, $screen ) {
-  if ( 'product' == $screen->id ) {
+/* Contextual help */
+
+function contextual_help( $contextual_help, $screen_id, $screen ) {
+  if ( 'article' == $screen->id ) {
 
     $contextual_help = '<h2>Products</h2>
-    <p>Products show the details of the items that we sell on the website. You can see a list of them on this page in reverse chronological order - the latest one we added is first.</p> 
-    <p>You can view/edit the details of each product by clicking on its name, or you can perform bulk actions using the dropdown menu and selecting multiple items.</p>';
+    <p>Articles</p> 
+    <p>You can view/edit the conent of each article by clicking on its name, or you can perform bulk actions using the dropdown menu and selecting multiple items.</p>';
 
-  } elseif ( 'edit-product' == $screen->id ) {
+  } elseif ( 'edit-article' == $screen->id ) {
 
-    $contextual_help = '<h2>Editing products</h2>
-    <p>This page allows you to view/modify product details. Please make sure to fill out the available boxes with the appropriate details (product image, price, brand) and <strong>not</strong> add these details to the product description.</p>';
+    $contextual_help = '<h2>Editing articles</h2>
+    <p>This page allows you to view/modify articles content.</p>';
 
   }
   return $contextual_help;
 }
-add_action( 'contextual_help', 'my_contextual_help', 10, 3 );
+add_action( 'contextual_help', 'contextual_help', 10, 3 );
 
-function my_taxonomies_product() {
+/* Taxonomies (categories) article */
+
+function taxonomies_article() {
     $labels = array(
-        'name'              => _x( 'Product Categories', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Product Category', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Product Categories' ),
-        'all_items'         => __( 'All Product Categories' ),
-        'parent_item'       => __( 'Parent Product Category' ),
-        'parent_item_colon' => __( 'Parent Product Category:' ),
-        'edit_item'         => __( 'Edit Product Category' ),
-        'update_item'       => __( 'Update Product Category' ),
-        'add_new_item'      => __( 'Add New Product Category' ),
-        'new_item_name'     => __( 'New Product Category' ),
-        'menu_name'         => __( 'Product Categories' ),
+        'name'              => _x( 'Article Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Article Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Article Categories' ),
+        'all_items'         => __( 'All Article Categories' ),
+        'parent_item'       => __( 'Parent Article Category' ),
+        'parent_item_colon' => __( 'Parent Article Category:' ),
+        'edit_item'         => __( 'Edit Article Category' ),
+        'update_item'       => __( 'Update Article Category' ),
+        'add_new_item'      => __( 'Add New Article Category' ),
+        'new_item_name'     => __( 'New Article Category' ),
+        'menu_name'         => __( 'Article Categories' ),
     );
     $args = array(
         'labels' => $labels,
         'hierarchical' => true,
     );
-    register_taxonomy( 'product_category', 'product', $args );
+    register_taxonomy( 'product_category', 'article', $args );
 }
-add_action( 'init', 'my_taxonomies_product', 0 );
+add_action( 'init', 'taxonomies_article', 0 );
+
+/* Meta Boxes */
+
